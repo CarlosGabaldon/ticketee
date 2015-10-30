2d12c62b9b706128e8ed8cbbc542bd7575d7d654
CarlosGabaldon <cgabaldon@gmail.com>
2015-10-30 10:29:52 -0700

    Made title dynamic based on selected project

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -2,14 +2,21 @@ require "rails_helper"
 
 RSpec.feature "Users can create new projects" do
   scenario "with valid attributes" do
+    project_name = "Sublime Text 3"
     visit "/"
 
     click_link "New Project"
 
-    fill_in "Name", with: "Sublime Text 3"
+    fill_in "Name", with: project_name
     fill_in "Description", with: "A text editor for everyone"
     click_button "Create Project"
 
     expect(page).to have_content "Project has been created."
+
+    project = Project.find_by(name: project_name)
+    expect(page.current_url).to eq project_url(project)
+
+    title = "#{project_name} - Projects - Ticketee"
+    expect(page).to have_title title
   end
 end
